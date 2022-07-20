5d48c1b70b28e260228fdeb1bb248a61b4f3c312
abidikhairi <abidikhairi.1337@gmail.com>
2022-07-20 15:43:30 +0200

    update gnn.py: DGCN::forward

--- project/models/gnn.py
+++ project/models/gnn.py
@@ -67,23 +67,26 @@ class DGCN(nn.Module):
         if skip_connection not in ['add', 'concat']:
             raise ValueError('skip_connection must be either "add" or "concat"')
 
+        if skip_connection == 'add':
+            assert nhids == proj_dim, 'nhids must be equal to proj_dim if skip_connection is "add"'
+
         self.skip_connection = skip_connection
         self.nhids = nhids if self.skip_connection == 'add' else nhids + proj_dim
 
         self.projection = nn.Linear(nfeats, proj_dim)
 
         self.conv1 = tgn.GCNConv(nfeats, nhids)
-        self.conv2 = tgn.GCNConv(nhids, nout)
+        self.conv2 = tgn.GCNConv(self.nhids, nout)
 
 
     def forward(self, edge_index, x):
-        x_porj = self.projection(x) if self.skip_connection == 'concat' else x
+        x_proj = self.projection(x)
 
         x = th.relu(self.conv1(x, edge_index))
         x = th.dropout(x, p=0.5, train=self.training)
-        
-        x = th.cat([x, x_porj]) if self.skip_connection == 'concat' else x + x_porj
-        
+
+        x = th.cat([x, x_proj]) if self.skip_connection == 'concat' else x + x_proj
+                
         x = self.conv2(x, edge_index)
 
         return x
