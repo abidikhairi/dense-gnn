dfb213a1acd9e6bc24b35ffea8b62ac2535f8c38
abidikhairi <abidikhairi.1337@gmail.com>
2022-07-21 15:24:24 +0200

    remove gnn baselines

--- project/models/gnn.py
+++ project/models/gnn.py
@@ -4,59 +4,6 @@ import torch.nn as nn
 import torch_geometric.nn as tgn
 
 
-class GCN(nn.Module):
-    """
-        Graph Convolutional Networks (GCN)
-    """
-    def __init__(self, nfeats, nhids, nout) -> None:
-        super().__init__()
-
-        self.conv1 = tgn.GCNConv(nfeats, nhids)
-        self.conv2 = tgn.GCNConv(nhids, nout)
-
-    def forward(self, edge_index, x):
-        x = th.relu(self.conv1(x, edge_index))
-        x = th.dropout(x, p=0.5, train=self.training)
-        x = self.conv2(x, edge_index)
-
-        return x
-
-
-class GAT(nn.Module):
-    """
-        Graph Attention Networks (GAT)
-    """
-    def __init__(self, nfeats, nhids, nout, nheads) -> None:
-        super().__init__()
-
-        self.conv1 = tgn.GATConv(nfeats, nhids, nheads)
-        self.conv2 = tgn.GATConv(nhids * nheads, nout, 1)
-        self.activation = th.nn.LeakyReLU(negative_slope=0.2)
-
-    def forward(self, edge_index, x):
-        x = self.activation(self.conv1(x, edge_index))
-        x = th.dropout(x, p=0.6, train=self.training)
-        x = self.conv2(x, edge_index)
-
-        return x
-
-
-class SGC(nn.Module):
-    """
-        Simplifying Graph Convolutional (SGC)
-    """
-    def __init__(self, nfeats, nouts, K=1) -> None:
-        super().__init__()
-
-        self.conv = tgn.SGConv(nfeats, nouts, K)
-
-    
-    def forward(self, edge_index, x):
-        x = self.conv(x, edge_index)
-
-        return x
-
-
 class DGCN(nn.Module):
     """
         Dense Graph Convolutional Networks (DGCN)
--- project/utils/models.py
+++ project/utils/models.py
@@ -1,17 +1,10 @@
-from ..models.gnn import GCN, GAT, SGC, DGCN
+from ..models.gnn import DGCN
 
 
 def load_model(name, args):
-    if name not in ['GCN', 'GAT', 'SGC', 'DGCN']:
+    if name not in ['DGCN']:
         raise ValueError('Invalid model name: {}'.format(name))
 
-    if name == 'GCN':
-        model = GCN(args.nfeats, args.nhids, args.nout)
-    elif name == 'GAT':
-        model = GAT(args.nfeats, args.nhids, args.nout, args.nheads)
-    elif name == 'SGC':
-        model = SGC(args.nfeats, args.nout, args.K)
-    elif name == 'DGCN':
-        model = DGCN(args.nfeats, args.nhids, args.nout, args.proj_dim, args.skip_connection)
+    model = DGCN(args.nfeats, args.nhids, args.nout, args.proj_dim, args.skip_connection)
     
     return model
