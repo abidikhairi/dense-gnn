8377020018bc01db2be6599d67792f476e450922
abidikhairi <abidikhairi.1337@gmail.com>
2022-09-05 17:58:49 +0200

    DGCN Encoder

--- project/models/gnn.py
+++ project/models/gnn.py
@@ -4,47 +4,42 @@ import dgl
 import torch as th
 import torch.nn as nn
 import torch_geometric.nn as tgn
-import dgl.nn.pytorch as dglnn
 
 
-class DGCNDGL(nn.Module):
-    def __init__(self, nfeats, nhids, nout, proj_dim=4) -> None:
-        super().__init__()
-        self.proj_dim = proj_dim
-
-        self.conv1 = dglnn.GraphConv(nfeats, nhids, allow_zero_in_degree=True)
-        self.conv2 = dglnn.GraphConv(nhids + self.proj_dim, nout, allow_zero_in_degree=True)
-        self.projection = nn.Linear(nfeats, proj_dim)
-
-
-    def forward(self, graph, feats):        
-        n_proj = self.projection(feats)
-        
-        h = self.conv1(graph, feats)
-        h = th.dropout(th.relu(h), p=0.5, train=self.training)
-        h = th.cat([h, n_proj], dim=1)
-
-        h = self.conv2(graph, h)
-
-        return h
-
-
-class DGCNGraphDGL(nn.Module):
-    def __init__(self, nfeats, nhids, nout, nclasses, proj_dim=4) -> None:
+class DGCNEncoder(nn.Module):
+    """
+        Dense Graph Convolutional Networks (DGCN)
+    """
+    def __init__(self, nfeats, nhids, proj_dim=4, skip_connection = 'add') -> None:
         super().__init__()
 
-        self.dgcn = DGCNDGL(nfeats, nhids, nclasses, proj_dim)
-        self.classify = nn.Linear(nout, nclasses)
+        if skip_connection not in ['add', 'concat']:
+            raise ValueError('skip_connection must be either "add" or "concat"')
 
-    def forward(self, graph, feats):
-        h = self.dgcn(graph, feats)
-        
-        # do the reduction here
-        graph.ndata['hn'] = h
-        hg = dgl.sum_nodes(graph, 'hn')
-        
-        return hg
+        if skip_connection == 'add':
+            raise ValueError('remove because of hyperparameter tuning')
 
+        self.skip_connection = skip_connection
+        self.nhids = nhids + proj_dim
+
+        self.projection = nn.Linear(nfeats, proj_dim, bias=False)
+
+        self.conv1 = tgn.GCNConv(nfeats, nhids)
+        self.conv2 = tgn.GCNConv(self.nhids, self.nhids)
+
+
+    def forward(self, edge_index, x):
+        x_proj = self.projection(x)
+
+        x = th.relu(self.conv1(x, edge_index))
+        x = th.dropout(x, p=0.5, train=self.training)
+
+        x = th.cat([x, x_proj], dim=1) if self.skip_connection == 'concat' else x + x_proj
+                
+        x = self.conv2(x, edge_index)
+
+        return th.cat([x, x_proj], dim=1)
+        
 
 class DGCN(nn.Module):
     """
